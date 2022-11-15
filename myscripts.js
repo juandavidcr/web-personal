22c17a76876d187c5c1ad51fb418bce6acc6d1fb
juandavid.contreras.contractor <juandavid.contreras.contractor@bbva.com>
2022-11-15 14:38:50 -0600

    First commit personal website

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+$("#ban").click(function() {
+    $("#text-banner").animate({
+    width: "50%",
+    opacity: 0.4,
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+  }, 1500 );
+});
